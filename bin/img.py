4f5231b824b53f820dc7ac93d671970fe5d8c8db
Stephen Kelly <stevekm@users.noreply.github.com>
2021-12-20 01:26:10 -0500

    remove old methods and test cases that are no longer needed

--- bin/img.py
+++ bin/img.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 """
 Module with helper functions for image processing tasks
+
+Notes
+---------------
+https://codegolf.stackexchange.com/questions/53621/force-an-average-on-an-image
+http://pythonicprose.blogspot.com/2009/09/python-find-average-rgb-color-for-image.html
+https://www.hackzine.org/getting-average-image-color-from-python.html
+https://stackoverflow.com/questions/6208980/sorting-a-list-of-rgb-triplets-into-a-spectrum
 """
 from __future__ import annotations # python 3.7+
 import sys
@@ -12,208 +19,6 @@ from pathlib import Path
 import argparse
 from typing import Generator, Tuple, List, Dict
 
-
-# ~~~~~ METHODS ~~~~~ #
-# these are mostly legacy functions developed a long time ago that are kept
-# because they still work
-def get_img_rgbs(image: str) -> Generator[Tuple[int, int, int], None, None]:
-    """
-    Get the RGB values for every pixel in an image
-
-    Usage
-    -----
-        pixels = [(red, green, blue) for red, green, blue in get_img_rgbs("color.jpg")]
-    """
-    img = Image.open(image).convert('RGB')
-    pixels = img.load()
-    for x in range(img.size[0]):
-        for y in range(img.size[1]):
-            yield(pixels[x, y])
-
-def get_img_rbg_hsv(
-    image: str
-    ) -> Generator[Tuple[int, int, int, float, float, int], None, None]:
-    """
-    Get the RGB and HSV values for every pixel in an image
-
-    Usage
-    -----
-        pixels = [(red, green, blue, hue, saturation, value) for red, green, blue, hue, saturation, value in get_img_rbg_hsv("color.jpg")]
-    """
-    for red, green, blue in get_img_rgbs(image):
-        hue, saturation, value = colorsys.rgb_to_hsv(red, green, blue)
-        yield(red, green, blue, hue, saturation, value)
-
-def load_csv_rgbhsv(
-    input_path: str
-    ) -> List[Tuple[str, str, str, str, str, str]]:
-    """
-    Load the RGB and HSV values from a .csv file
-
-    TODO: deprecate this !!
-    """
-    pixels = []
-    with open(input_path) as f:
-        reader = csv.reader(f)
-        for row in reader:
-            red = row[0]
-            green = row[1]
-            blue = row[2]
-            hue = row[3]
-            saturation = row[4]
-            value = row[5]
-            pixels.append((red, green, blue, hue, saturation, value))
-    return(pixels)
-
-
-def get_img_avg_rgb(
-    image: str,
-    ignore_csvfile: str = None,
-    ignore_list: List[Dict] = None,
-    ignore_avg: Avg = None,
-    _verbose: bool = True
-    ) -> Dict:
-    """
-    Gets the average RGB of an image
-
-    Notes
-    ---------------
-    https://codegolf.stackexchange.com/questions/53621/force-an-average-on-an-image
-    http://pythonicprose.blogspot.com/2009/09/python-find-average-rgb-color-for-image.html
-    https://www.hackzine.org/getting-average-image-color-from-python.html
-    https://stackoverflow.com/questions/6208980/sorting-a-list-of-rgb-triplets-into-a-spectrum
-    """
-    ignore_pixels = set() # ['RGB', ... ] ; ['255255255', ... ]
-    # load pixels to ignore from a csv file
-    # NOTE: this is a really bad method that should be deprecated or rewritten
-    if ignore_csvfile != None:
-        ignore_pixels_list = load_csv_rgbhsv(input_path = ignore_csvfile)
-        ignore_pixels_list = [ (tup[0], tup[1], tup[2]) for tup in ignore_pixels_list ]
-        for tup in ignore_pixels_list:
-            id = "{0}.{1}.{2}".format(tup[0], tup[1], tup[2])
-            ignore_pixels.add(id)
-        if _verbose:
-            print("Loaded {0} ignore pixels".format(len(ignore_pixels)))
-
-    # load pixels to ignore from a list of dicts or Avg objects
-    if ignore_list != None:
-        for item in ignore_list:
-            if isinstance(item, dict):
-                id = "{0}.{1}.{2}".format(item['red'], item['green'], item['blue'])
-                ignore_pixels.add(id)
-            elif hasattr(item, 'to_dict'):
-                d = item.to_dict()
-                id = "{0}.{1}.{2}".format(d['red'], d['green'], d['blue'])
-                ignore_pixels.add(id)
-        if _verbose:
-            print("Loaded {0} ignore pixels".format(len(ignore_pixels)))
-
-    # load pixels to ignore from an Avg instance
-    if ignore_avg != None:
-        id = "{0}.{1}.{2}".format(ignore_avg.red, ignore_avg.green, ignore_avg.blue)
-        ignore_pixels.add(id)
-        if _verbose:
-            print("Loaded {0} ignore pixels".format(len(ignore_pixels)))
-
-    img = Image.open(image).convert('RGB')
-    pixels = img.load()
-    size_x = img.size[0]
-    size_y = img.size[1]
-    avg = {
-        'red': 0,
-        'green': 0,
-        'blue': 0,
-        'pixels_total' : size_x * size_y,
-        'pixels_counted' : 0,
-        'path': image
-        }
-
-    if _verbose:
-        print("Loaded image: {0} total pixels".format(avg['pixels_total']))
-
-    # add up the RGB values for all pixels
-    for x in range(img.size[0]): # iterate over all x pixels
-        for y in range(img.size[1]): # iterate over all y pixels
-            red = pixels[x, y][0]
-            green = pixels[x, y][1]
-            blue = pixels[x, y][2]
-
-            # skip the pixel if it matches one of the ignored pixels
-            if len(ignore_pixels) > 0:
-                id = "{0}.{1}.{2}".format(red, green, blue)
-                if id not in ignore_pixels:
-                    avg['red'] += red
-                    avg['green'] += green
-                    avg['blue'] += blue
-                    avg['pixels_counted'] += 1
-            else:
-                avg['red'] += red
-                avg['green'] += green
-                avg['blue'] += blue
-                avg['pixels_counted'] += 1
-    # calculate averages
-    avg['red'] = avg['red'] // avg['pixels_counted']
-    avg['green'] = avg['green'] // avg['pixels_counted']
-    avg['blue'] = avg['blue'] // avg['pixels_counted']
-
-    # convert to HSV
-    hue, saturation, value = colorsys.rgb_to_hsv(avg['red'], avg['green'], avg['blue'])
-    avg['hue'] = hue
-    avg['saturation'] = saturation
-    avg['value'] = value
-
-    # calculate percent
-    avg['pixels_pcnt'] = round((float(avg['pixels_counted']) / float(avg['pixels_total'])) * 100, 1)
-
-    return(avg)
-
-
-def get_avgs(
-        images: List[str],
-        sort_key: str = "hue",
-        parallel: int = 1,
-        *args,
-        **kwargs) -> List[Dict]:
-    """
-    Get the average RBG HSV values for all the images in the list
-    With optional parallel processing
-    Sort the output list based on key
-    """
-    avgs = []
-    # run in single-threaded mode
-    if parallel == 1:
-        for image in images:
-            avgs.append(get_img_avg_rgb(image, *args, **kwargs))
-
-    # run in multi-threaded mode
-    else:
-        pool = Pool(int(parallel))
-
-        # generate the aysnc result instances
-        results = []
-        for image in images:
-            result = pool.apply_async(get_img_avg_rgb, args=(image, *args), kwds=kwargs)
-            result_tup = (image, result)
-            results.append(result_tup)
-
-         # get each result
-        for result_tup in results:
-            image, result = result_tup
-            avg = result.get()
-            avgs.append(avg)
-
-    if sort_key:
-        avgs = sorted(avgs, key = lambda avg: avg[sort_key])
-
-    return(avgs)
-
-
-
-
-
-# ~~~~~ OBJECTS ~~~~~ #
-# custom object class for easily generating image averages
-# use this class instead of calling the previous methods directly !!
 class Avg(object):
     """
     Holds the attributes of image's average RGB HSV values
@@ -474,14 +279,12 @@ def main():
     _print.add_argument('--threads', dest = 'threads', default = 4, help = 'Number of files to process in parallel')
     _print.add_argument('--ignore', dest = 'ignore_file', default = None, help = 'File with pixels that should be ignored when calculating averages')
     _print.set_defaults(func = print_from_path)
-
+    """
+    $ bin/img.py print assets/ --threads 6 --ignore ignore-pixels-white.jpg
+    """
 
     args = parser.parse_args()
     args.func(**vars(args))
-
-
-
-
 
 if __name__ == '__main__':
     main()
--- bin/test_img.py
+++ bin/test_img.py
@@ -7,7 +7,7 @@ import unittest
 import shutil
 from tempfile import mkdtemp
 import colorsys
-from img import get_img_rgbs, get_img_avg_rgb, get_img_rbg_hsv, get_avgs
+# from img import get_img_rgbs, get_img_avg_rgb, get_img_rbg_hsv, get_avgs
 from img import Avg
 
 # get paths to the fixture image files
@@ -28,110 +28,6 @@ white_expected = {'red': 255, 'green': 255, 'blue': 255, 'pixels_total': 1, 'pix
 
 green_expected = {'red': 1, 'green': 255, 'blue': 1, 'pixels_total': 1, 'pixels_counted': 1, 'hue': 0.3333333333333333, 'saturation': 0.996078431372549, 'value': 255, 'pixels_pcnt': 100.0, 'path': green_jpg}
 
-class TestIMG(unittest.TestCase):
-    def setUp(self):
-        """this gets run for each test case"""
-        self.tmpdir = mkdtemp()
-
-    def tearDown(self):
-        """this gets run for each test case"""
-        shutil.rmtree(self.tmpdir)
-
-    def test_get_img_rgbs(self):
-        """
-        Make sure that we can get the correct RGB values for sample images
-        """
-        # test multiple colors
-        pixels = [(red, green, blue) for red, green, blue in get_img_rgbs(colors_jpg)]
-        expected = [(254, 0, 0), (255, 255, 103), (1, 255, 2), (1, 0, 254)]
-        self.assertEqual(pixels, expected)
-
-        # test just black
-        pixels = [(red, green, blue) for red, green, blue in get_img_rgbs(black_jpg)]
-        expected = [(0, 0, 0)]
-        self.assertEqual(pixels, expected)
-
-        # test just white
-        pixels = [(red, green, blue) for red, green, blue in get_img_rgbs(white_jpg)]
-        expected = [(255, 255, 255)]
-        self.assertEqual(pixels, expected)
-
-
-    def test_get_img_rbg_hsv(self):
-        """
-        Test that the correct RGB HSV values are returned
-        """
-        # test just black
-        pixels = [(red, green, blue, hue, saturation, value) for red, green, blue, hue, saturation, value in get_img_rbg_hsv(black_jpg)]
-        expected = [(0, 0, 0, 0.0, 0.0, 0)]
-        self.assertEqual(pixels, expected)
-        # test just green
-        pixels = [(red, green, blue, hue, saturation, value) for red, green, blue, hue, saturation, value in get_img_rbg_hsv(green_jpg)]
-        expected = [(1, 255, 1, 0.3333333333333333, 0.996078431372549, 255)]
-        self.assertEqual(pixels, expected)
-
-
-    def test_get_img_avg_rgb(self):
-        """
-        Make sure the correct average pixel values are returned for an image
-        """
-        # get average for mixed colors
-        avg = get_img_avg_rgb(colors_jpg, _verbose = False)
-        self.assertDictEqual(avg, colors_expected)
-
-        # write out a tmp file that should equate to green
-        green_csv = os.path.join(self.tmpdir, "green.csv")
-        values = [1, 255, 2, 0.5, 0, 255]
-        with open(green_csv, "w") as f:
-            f.write(','.join([ str(v) for v in values ]))
-        # get the average after subtracting the green values
-        avg = get_img_avg_rgb(colors_jpg, ignore_csvfile = green_csv,  _verbose = False)
-        self.assertDictEqual(avg, colors_minus_green_expected)
-
-        # use a dict of values for the ignore pixel list
-        ignore_list = [{'red': 1, 'green': 255, 'blue': 2}]
-        avg = get_img_avg_rgb(colors_jpg, ignore_list = ignore_list,  _verbose = False)
-        self.assertDictEqual(avg, colors_minus_green_expected)
-
-        # use an Avg instance to ignore from
-        ignore_avg = Avg.from_dict({'red': 1, 'green': 255, 'blue': 2, 'hue': 0, 'saturation': 0, 'value': 0, 'path': 'foo', 'pixels_total': 0, 'pixels_counted': 0, 'pixels_pcnt': 0})
-        avg = get_img_avg_rgb(colors_jpg, ignore_avg = ignore_avg,  _verbose = False)
-        self.assertDictEqual(avg, colors_minus_green_expected)
-
-    def test_get_avgs(self):
-        """
-        Check that the correct averages are returned for image files
-        """
-        # get just average of colors.jpg
-        avgs = get_avgs([colors_jpg], _verbose = False)
-        expected = [colors_expected]
-        self.assertEqual(avgs, expected)
-
-        # get average while ignoring green pixels
-        # write out a tmp file that should equate to green
-        green_csv = os.path.join(self.tmpdir, "green.csv")
-        values = [1, 255, 2, 0.5, 0, 255]
-        with open(green_csv, "w") as f:
-            f.write(','.join([ str(v) for v in values ]))
-        avgs = get_avgs([colors_jpg], ignore_csvfile = green_csv, _verbose = False)
-        expected = [colors_minus_green_expected]
-        self.assertEqual(avgs, expected)
-
-        # get averages from multiple images
-        # default sort key = hue
-        avgs = get_avgs([colors_jpg, green_jpg, white_jpg], _verbose = False)
-        expected = [white_expected, colors_expected, green_expected]
-        self.assertEqual(avgs, expected)
-
-        # with no sort key
-        avgs = get_avgs([colors_jpg, green_jpg, white_jpg], sort_key = False, _verbose = False)
-        expected = [colors_expected, green_expected, white_expected]
-        self.assertEqual(avgs, expected)
-
-        # run multiple in parallel
-        avgs = get_avgs([colors_jpg, green_jpg, white_jpg], parallel = 2, _verbose = False)
-        expected = [white_expected, colors_expected, green_expected]
-        self.assertEqual(avgs, expected)
 
 class TestAvg(unittest.TestCase):
     def test_avg(self):
@@ -166,6 +62,13 @@ class TestAvg(unittest.TestCase):
         for i, e in enumerate([colors_expected, green_expected, white_expected]):
             for key in e.keys():
                 self.assertEqual(getattr(avgs[i], key), e[key])
+
+        # with sort key
+        avgs = Avg().from_list(paths = paths, parallel = 1)
+        for i, e in enumerate([white_expected, colors_expected, green_expected]):
+            for key in e.keys():
+                self.assertEqual(getattr(avgs[i], key), e[key])
+
 
 
 
